b1495029a8947f81f71a682225331f89b6d0e9ab
Yasuhiro Osajima <yasuhiro-osajima@gmo.jp>
2020-03-11 02:41:02 +0900

    update circle domain object

--- internal/domain/models/circle/circle.go
+++ internal/domain/models/circle/circle.go
@@ -1,6 +1,10 @@
 package circle
 
-import "go-layered-architecture-practice/internal/domain/models/user"
+import (
+	"errors"
+
+	"go-layered-architecture-practice/internal/domain/models/user"
+)
 
 type circle struct {
 	id      circleId
@@ -13,10 +17,34 @@ func NewCircle(id circleId, name circleName, owner user.User, members []user.Use
 	return &circle{id, name, owner, members}
 }
 
+func (c circle) Id() circleId {
+	return c.id
+}
+
+func (c circle) Name() circleName {
+	return c.name
+}
+
+func (c circle) Owner() user.User {
+	return c.owner
+}
+
+func (c circle) Members() []user.User {
+	return c.members
+}
+
 func (c circle) IsFull() bool {
 	return c.CountMembers() >= 30
 }
 
 func (c circle) CountMembers() int {
 	return len(c.members) + 1
+}
+
+func (c *circle) Join(user user.User) error {
+	if c.IsFull() {
+		return errors.New("circle has full count of members")
+	}
+	c.members = append(c.members, user)
+	return nil
 }
