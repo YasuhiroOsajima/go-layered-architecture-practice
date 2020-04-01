414018f2826d8ac4c30e93bf028a48d002e763df
Yasuhiro Osajima <yasuhiro-osajima@gmo.jp>
2020-04-01 21:52:42 +0900

    add circle data

--- pkg/circle/circle_data.go
+++ pkg/circle/circle_data.go
@@ -1,1 +1,17 @@
 package circle
+
+import (
+	circle_model "go-layered-architecture-practice/internal/domain/models/circle"
+	"go-layered-architecture-practice/pkg/user"
+)
+
+type CircleData struct {
+	Id      string
+	Name    string
+	Owner   user.UserData
+	Members []user.UserData
+}
+
+func NewCircleData(circle *circle_model.Circle, owner user.UserData, members []user.UserData) CircleData {
+	return CircleData{circle.Id().AsString(), circle.Name().AsString(), owner, members}
+}
