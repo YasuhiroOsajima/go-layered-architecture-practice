d056626e9df5a38ea991ec306da9a11d19a1a4d3
Yasuhiro Osajima <yasuhiro-osajima@gmo.jp>
2020-04-02 21:32:04 +0900

    update specification

--- internal/domain/models/circle/recommended_circle_specification.go
+++ internal/domain/models/circle/recommended_circle_specification.go
@@ -1,1 +1,4 @@
 package circle
+
+// If domain object needs repository, the process should be write in specification method in each 'specification' file.
+// Entities and value objects should not contain processes which uses repository.
