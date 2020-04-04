ecc6fb9f7078da12095282277e19a0ac5648c057
Yasuhiro Osajima <yasuhiro-osajima@gmo.jp>
2020-04-04 18:29:24 +0900

    add specification interface

--- internal/domain/shared/specification_interface.go
+++ internal/domain/shared/specification_interface.go
@@ -1,1 +1,6 @@
 package shared
+
+// Specification objects can be taken as function args by repository objects
+// like complex read data SQL to avoid 1+N performance problem.
+// In these cases, repository functions should take this interface object
+// to avoid dependency to upper side layer.
